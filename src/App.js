7a5426ade1cf6373e4b76de9b1760a0be0ad8689
Bikash Thapa <thapabikash48@gmail.com>
2023-08-22 14:21:16 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [currentTime,setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      document.title = "Current Time is " + now.toLocaleTimeString();
+      setCurrentTime(now);
+    }, 1000);
+
+    return () => clearInterval(interval);
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Your Current Time is:</p>
+      <h1><code>{currentTime.toLocaleTimeString()}</code></h1>
     </div>
   );
 }
